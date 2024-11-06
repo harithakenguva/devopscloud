c5904eaba8bc3e204701d1161baa4eb361c89f3a
haritha <haritha1@gmail.com>
2024-11-06 09:58:53 +0530

    modified app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import boto3
+client = boto3.client('ec2')
+response = client.run_instances(
+   ImageId='ami-06b21ccaeff8cd686',
+   InstanceType='t2.medium',
+   KeyName='eksk8s',
+   Maxcount=1,
+   MinCount=1
+)
